df7afcf9e79a54603b121e13574e39e9f437d1b3
Michael Millar <mcmelord@gmail.com>
2023-11-28 08:03:02 -0700

    start building printers

---
+++ src/main/java/edu/unm/service/BallotPrinter.java
@@ -0,0 +1,16 @@
+package edu.unm.service;
+
+import edu.unm.entity.Ballot;
+
+import java.io.IOException;
+
+/**
+ * created by:
+ * author: MichaelMillar
+ */
+public class BallotPrinter {
+
+    public void print(Ballot ballot) throws IOException {
+
+    }
+}
