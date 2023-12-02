c7d5921ca363f899e954c54a76ffcc88c00989f3
Mike Millar <mmillar@cnm.edu>
2023-12-02 13:43:09 -0700

    create user service template

---
+++ src/main/java/edu/unm/service/UserService.java
@@ -0,0 +1,25 @@
+package edu.unm.service;
+
+import edu.unm.entity.Elector;
+import edu.unm.entity.Staff;
+
+public class UserService {
+
+    public static Elector verityElector(String id) {
+        // TODO
+        return null;
+    }
+
+    public static Staff verifyStaff(String id, String password) {
+        // TODO
+        return null;
+    }
+
+    public static boolean registerElector(Elector elector) {
+        // TODO
+        return false;
+    }
+
+
+
+}
