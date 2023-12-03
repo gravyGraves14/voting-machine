2073bf1e96bee2abaa6e54c1558bdaacecd02e7d
Michael Millar <mcmelord@gmail.com>
2023-12-03 13:16:29 -0700

    fixed formatter, was using wrong class

--- src/main/java/edu/unm/utils/DateUtils.java
+++ src/main/java/edu/unm/utils/DateUtils.java
@@ -2,6 +2,7 @@ package edu.unm.utils;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * created by:
@@ -12,8 +13,8 @@ public class DateUtils {
     // STATIC USE ONLY
     private DateUtils() {}
 
-    private static final SimpleDateFormat formatter =
-            new SimpleDateFormat("MM-dd-yyyy-HH-mm-ss");
+    private static final DateTimeFormatter formatter =
+            DateTimeFormatter.ofPattern("MM-dd-yyyy-HH-mm-ss");
 
     public static String formatLocalDateTime(LocalDateTime dateTime) {
         return formatter.format(dateTime);
